963305f68066f64213d388ca08ceffc103868d2c
michelleheh <xiaoyu.he22@gmail.com>
2016-06-24 15:17:33 -0700

    Clean up app.jsx

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,16 +1,39 @@
 import React from 'react';
 
 import Header from './header.jsx';
-import ToDoList from './toDoList.jsx';
 import MainMap from './mainMap.jsx';
 import Footer from './footer.jsx';
 
+import initialData from '../initialData.js'
+
 export default class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = initialData;
+  }
+
+  setCenter(center){
+    this.setState({
+      currentCenter: center
+    });
+  }
+
+  showSpots(spots){
+    this.setState({
+      currentSpots: spots
+    });
+  }
+
   render() {
     return (
       <div>
-        <Header />
-        <MainMap />
+        <Header 
+          currentLocation={this.state.currentLocation} 
+          showSpots={this.showSpots.bind(this)}
+          setCenter={this.setCenter.bind(this)} />
+        <MainMap 
+          currentCenter={this.state.currentCenter} 
+          spots={this.state.currentSpots}/>
       </div>
     );
   }
