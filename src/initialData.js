5316c069019395805227739220e8e49d5faeb6b0
michelleheh <xiaoyu.he22@gmail.com>
2016-06-24 15:22:08 -0700

    Extract initial data

---
+++ src/initialData.js
@@ -0,0 +1,227 @@
+module.exports = {
+  currentLocation: 'Moscone Center, SF',
+  currentCenter: {lat: 37.784173, lng: -122.401557},
+  currentSpots: [
+    {
+      "id": 909,
+      "lat": "37.782812",
+      "lng": "-122.402299",
+      "name": "204-02140",
+      "cost_per_minute": "0.50",
+      "max_reserve_time_mins": 120,
+      "min_reserve_time_mins": 10,
+      "is_reserved": false,
+      "reserved_until": null
+    },
+    {
+      "id": 1994,
+      "lat": "37.783037",
+      "lng": "-122.402840",
+      "name": "470-08050",
+      "cost_per_minute": "0.50",
+      "max_reserve_time_mins": 120,
+      "min_reserve_time_mins": 10,
+      "is_reserved": false,
+      "reserved_until": null
+    },
+    {
+      "id": 255,
+      "lat": "37.782907",
+      "lng": "-122.403004",
+      "name": "470-08110",
+      "cost_per_minute": "0.50",
+      "max_reserve_time_mins": 120,
+      "min_reserve_time_mins": 10,
+      "is_reserved": false,
+      "reserved_until": null
+    },
+    {
+      "id": 494,
+      "lat": "37.783383",
+      "lng": "-122.399592",
+      "name": "418-07200",
+      "cost_per_minute": "0.50",
+      "max_reserve_time_mins": 120,
+      "min_reserve_time_mins": 10,
+      "is_reserved": false,
+      "reserved_until": null
+    },
+    {
+      "id": 256,
+      "lat": "37.782630",
+      "lng": "-122.403355",
+      "name": "470-08270",
+      "cost_per_minute": "0.50",
+      "max_reserve_time_mins": 120,
+      "min_reserve_time_mins": 10,
+      "is_reserved": false,
+      "reserved_until": null
+    },
+    {
+      "id": 477,
+      "lat": "37.783631",
+      "lng": "-122.398891",
+      "name": "203-03020",
+      "cost_per_minute": "0.50",
+      "max_reserve_time_mins": 120,
+      "min_reserve_time_mins": 10,
+      "is_reserved": true,
+      "reserved_until": "2016-06-11T00:32:03.323346Z"
+    },
+    {
+      "id": 800,
+      "lat": "37.784889",
+      "lng": "-122.398806",
+      "name": "525-00513",
+      "cost_per_minute": "0.50",
+      "max_reserve_time_mins": 120,
+      "min_reserve_time_mins": 10,
+      "is_reserved": false,
+      "reserved_until": null
+    },
+    {
+      "id": 2005,
+      "lat": "37.783919",
+      "lng": "-122.398607",
+      "name": "418-06750",
+      "cost_per_minute": "0.50",
+      "max_reserve_time_mins": 120,
+      "min_reserve_time_mins": 10,
+      "is_reserved": false,
+      "reserved_until": null
+    },
+    {
+      "id": 1610,
+      "lat": "37.783954",
+      "lng": "-122.398562",
+      "name": "418-06730",
+      "cost_per_minute": "0.50",
+      "max_reserve_time_mins": 120,
+      "min_reserve_time_mins": 10,
+      "is_reserved": false,
+      "reserved_until": null
+    },
+    {
+      "id": 1382,
+      "lat": "37.786233",
+      "lng": "-122.399960",
+      "name": "580-01570",
+      "cost_per_minute": "0.50",
+      "max_reserve_time_mins": 120,
+      "min_reserve_time_mins": 10,
+      "is_reserved": false,
+      "reserved_until": null
+    },
+    {
+      "id": 772,
+      "lat": "37.785660",
+      "lng": "-122.399146",
+      "name": "463-00060",
+      "cost_per_minute": "0.50",
+      "max_reserve_time_mins": 120,
+      "min_reserve_time_mins": 10,
+      "is_reserved": false,
+      "reserved_until": null
+    },
+    {
+      "id": 1795,
+      "lat": "37.786041",
+      "lng": "-122.399324",
+      "name": "470-06420",
+      "cost_per_minute": "0.50",
+      "max_reserve_time_mins": 120,
+      "min_reserve_time_mins": 10,
+      "is_reserved": false,
+      "reserved_until": null
+    },
+    {
+      "id": 1566,
+      "lat": "37.786762",
+      "lng": "-122.401239",
+      "name": "568-06650",
+      "cost_per_minute": "0.50",
+      "max_reserve_time_mins": 120,
+      "min_reserve_time_mins": 10,
+      "is_reserved": true,
+      "reserved_until": "2016-06-10T21:46:10.018831Z"
+    },
+    {
+      "id": 1493,
+      "lat": "37.785127",
+      "lng": "-122.398472",
+      "name": "463-00340",
+      "cost_per_minute": "0.50",
+      "max_reserve_time_mins": 120,
+      "min_reserve_time_mins": 10,
+      "is_reserved": false,
+      "reserved_until": null
+    },
+    {
+      "id": 1445,
+      "lat": "37.781605",
+      "lng": "-122.400792",
+      "name": "204-03200",
+      "cost_per_minute": "0.50",
+      "max_reserve_time_mins": 120,
+      "min_reserve_time_mins": 10,
+      "is_reserved": false,
+      "reserved_until": null
+    },
+    {
+      "id": 1446,
+      "lat": "37.781592",
+      "lng": "-122.400775",
+      "name": "204-03220",
+      "cost_per_minute": "0.50",
+      "max_reserve_time_mins": 120,
+      "min_reserve_time_mins": 10,
+      "is_reserved": false,
+      "reserved_until": null
+    },
+    {
+      "id": 391,
+      "lat": "37.782224",
+      "lng": "-122.403869",
+      "name": "470-08530",
+      "cost_per_minute": "0.50",
+      "max_reserve_time_mins": 120,
+      "min_reserve_time_mins": 10,
+      "is_reserved": false,
+      "reserved_until": null
+    },
+    {
+      "id": 1794,
+      "lat": "37.786287",
+      "lng": "-122.399166",
+      "name": "582-01480",
+      "cost_per_minute": "0.50",
+      "max_reserve_time_mins": 120,
+      "min_reserve_time_mins": 10,
+      "is_reserved": true,
+      "reserved_until": "2016-06-10T22:05:26.103462Z"
+    },
+    {
+      "id": 1932,
+      "lat": "37.781321",
+      "lng": "-122.401894",
+      "name": "418-08330",
+      "cost_per_minute": "0.50",
+      "max_reserve_time_mins": 120,
+      "min_reserve_time_mins": 10,
+      "is_reserved": false,
+      "reserved_until": null
+    },
+    {
+      "id": 1510,
+      "lat": "37.786376",
+      "lng": "-122.399090",
+      "name": "582-01470",
+      "cost_per_minute": "0.50",
+      "max_reserve_time_mins": 120,
+      "min_reserve_time_mins": 10,
+      "is_reserved": false,
+      "reserved_until": null
+    }
+  ]
+};
+
