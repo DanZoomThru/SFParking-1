7cff93db493241f8aa089ff9ac22d725ca282df6
michelleheh <xiaoyu.he22@gmail.com>
2016-06-24 15:18:53 -0700

    Add get request methods in header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,6 +5,30 @@ export default class Header extends React.Component {
     super(props);
   }
 
+  getSpots(location){
+    $.getJSON({
+      url  : `http://ridecellparking.herokuapp.com/api/v1/parkinglocations/search?lat=${location.lat}&lng=${location.lng}`,
+      success : ( data, textStatus ) => {
+        this.props.showSpots(data);
+      }
+    });
+  }
+
+  getCurrentLatLng(){
+    $.getJSON({
+      url  : 'https://maps.googleapis.com/maps/api/geocode/json',
+      data : {
+        sensor  : false,
+        address : this.props.currentLocation
+      },
+      success : ( data, textStatus ) => {
+        console.log('--> data:', data.results)
+        this.props.setCenter(data.results[0].geometry.location);
+        this.getSpots(data.results[0].geometry.location);
+      }
+    });
+  }
+
   render() {
     return (
       <nav className="navbar navbar-default navbar-fixed-top">
@@ -13,7 +37,9 @@ export default class Header extends React.Component {
             <div className="input-group">
               <input type="text" className="form-control" placeholder="Search for parking spots near..." />
               <span className="input-group-btn">
-                <button className="btn btn-default" type="button">Go!</button>
+                <button className="btn btn-default" type="button" 
+                  onClick={
+                    this.getCurrentLatLng.bind(this)}>Find Parking Spots!</button>
               </span>
             </div>
           </div>
