3e0d506de0ffe21fdd75cda400640ec7dcdc4146
michelleheh <xiaoyu.he22@gmail.com>
2016-06-24 15:20:30 -0700

    Update marker styling

--- src/components/mainMap.jsx
+++ src/components/mainMap.jsx
@@ -2,59 +2,35 @@ import React, {PropTypes, Component} from 'react';
 import GoogleMap from 'google-map-react';
 
 import Marker from './marker.jsx'
+import Center from './center.jsx'
 
 export default class MainMap extends Component {
   static defaultProps = {
-    zoom: 15,
+    zoom: 17,
   };
 
   constructor(props) {
     super(props);
-
-    this.state = {
-      center: {lat: 37.7730933, lng: -122.4125351},
-      spots: [
-      {
-          "id": 2017,
-          "lat": "37.773090",
-          "lng": "-122.412530",
-          "name": "",
-          "cost_per_minute": "0.50",
-          "max_reserve_time_mins": 120,
-          "min_reserve_time_mins": 10,
-          "is_reserved": false,
-          "reserved_until": null
-        },
-        {
-          "id": 2020,
-          "lat": "37.774900",
-          "lng": "-122.419400",
-          "name": "",
-          "cost_per_minute": "0.50",
-          "max_reserve_time_mins": 120,
-          "min_reserve_time_mins": 10,
-          "is_reserved": false,
-          "reserved_until": null
-        }
-      ]
-    };
   }
 
   render() {
     return (
       <div className="map">
         <GoogleMap
-          defaultCenter={this.state.center}
+          center={this.props.currentCenter}
           defaultZoom={this.props.zoom}>
           {
-            this.state.spots.map((spot) => {
+            this.props.spots.map((spot) => {
               return ( 
-                <Marker key={spot.id}
+                <Marker key={spot.id} spot={spot}
                   lat={spot.lat} 
                   lng={spot.lng} text={spot.id} />
                 )
             })
           }
+          <Center
+            lat={this.props.currentCenter.lat} 
+            lng={this.props.currentCenter.lng} />
         </GoogleMap>
       </div>
     );
--- src/components/marker.jsx
+++ src/components/marker.jsx
@@ -13,24 +13,21 @@ export default class Marker extends Component {
 
   render() {
 
-    const K_WIDTH = 40;
-    const K_HEIGHT = 40;
+    const K_WIDTH = 20;
+    const K_HEIGHT = 20;
 
     const greatPlaceStyle = {
-      // initially any map object has left top corner at lat lng coordinates
-      // it's on you to set object origin to 0,0 coordinates
-      // position: 'absolute',
       width: K_WIDTH,
       height: K_HEIGHT,
       left: -K_WIDTH / 2,
       top: -K_HEIGHT / 2,
 
-      border: '5px solid #f44336',
+      border: '2px solid #f44336',
       borderRadius: K_HEIGHT,
       backgroundColor: 'white',
       textAlign: 'center',
       color: '#3f51b5',
-      fontSize: 16,
+      fontSize: 10,
       fontWeight: 'bold',
       padding: 4
     };
@@ -42,13 +39,16 @@ export default class Marker extends Component {
         </div>
 
         <Overlay
+          key={this.props.spot.id}
           show={this.state.show}
           target={()=> ReactDOM.findDOMNode(this.state.target)}
           placement="top"
           container={this}
           containerPadding={20}
         >
-          <Detail />
+          <Detail 
+            key={this.props.spot.id}
+            spot={this.props.spot}/>
         </Overlay>
       </div>
     );
