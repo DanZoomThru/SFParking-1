caa71ee7d196ea6b2e5cc105e6e9c42e388510fe
michelleheh <xiaoyu.he22@gmail.com>
2016-06-24 15:21:32 -0700

    Add center component

---
+++ src/components/center.jsx
@@ -0,0 +1,40 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+import Detail from './detail.jsx'
+
+export default class Center extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+
+    const K_WIDTH = 20;
+    const K_HEIGHT = 20;
+
+    const greatPlaceStyle = {
+      width: K_WIDTH,
+      height: K_HEIGHT,
+      left: -K_WIDTH / 2,
+      top: -K_HEIGHT / 2,
+
+      border: '2px solid green',
+      borderRadius: K_HEIGHT,
+      backgroundColor: 'white',
+      textAlign: 'center',
+      color: '#3f51b5',
+      fontSize: 10,
+      fontWeight: 'bold',
+      padding: 4
+    };
+
+    return (
+      <div>
+        <div style={greatPlaceStyle}>
+          {'me'}
+        </div>
+      </div>
+    );
+  }
+}
