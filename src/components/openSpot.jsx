4f672926a1554a70eb590d5f6c9ed6740e87f870
michelleheh <xiaoyu.he22@gmail.com>
2016-06-25 03:02:31 -0700

    Rename files

---
+++ src/components/openSpot.jsx
@@ -0,0 +1,50 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+import { OverlayTrigger } from 'react-bootstrap';
+import OpenSpotPopover from './openSpotPopover.jsx'
+import ConfirmModal from './confirmModal.jsx';
+
+export default class OpenSpot extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  markerSelected(e) {
+    this.props.setCenter({lat: Number(this.props.spot.lat), lng: Number(this.props.spot.lng)});
+  }
+
+  render() {
+    const boarderRadius = 16;
+    const dotRadius = 10;
+
+    const spotStyle = {
+      position: 'absolute',
+      width: boarderRadius * 2,
+      height: boarderRadius * 2,
+      left: -boarderRadius,
+      top: -boarderRadius,
+      border: '2px solid #00e6e6',
+      borderRadius: boarderRadius,
+    };
+
+    const dotStyle = {
+      position: 'relative',
+      width: dotRadius * 2,
+      height: dotRadius * 2,
+      left: 4,
+      top: 4,
+      borderRadius: dotRadius,
+      backgroundColor: '#00e6e6',
+    };
+
+    return (
+      <OverlayTrigger trigger="click" rootClose placement="top" overlay={
+        <OpenSpotPopover showSpots={this.props.showSpots} addToReserved={this.props.addToReserved} spot={this.props.spot} target={this.props.target} /> }>
+        <div className="marker" style={spotStyle} onClick={this.markerSelected.bind(this)}>
+          <div style={dotStyle}></div>
+        </div>
+      </OverlayTrigger>
+    )
+  }
+}
---
+++ src/components/openSpotPopover.jsx
@@ -0,0 +1,71 @@
+import React, {Component} from 'react';
+
+import { Popover } from 'react-bootstrap';
+import ConfirmModal from './confirmModal.jsx';
+
+export default class OpenSpotPopover extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      modalShown: false
+    }
+  }
+
+  getSpots(location){
+    $.getJSON({
+      url  : `http://ridecellparking.herokuapp.com/api/v1/parkinglocations/search?lat=${location.lat}&lng=${location.lng}`,
+      success : ( data, textStatus ) => {
+        this.props.showSpots(data);
+      }
+    });
+  }
+
+  requestSpot(id) {
+    $.ajax({
+      url: `http://ridecellparking.herokuapp.com/api/v1/parkinglocations/${id}/reserve/`,
+      method: "POST",
+      dataType: "JSON",
+      success: ( data, textStatus ) => {
+        this.setState({modalShown: true});
+      }
+    });
+  }
+
+  reserveSpot(){
+    this.props.addToReserved(this.props.spot);
+    this.requestSpot(this.props.spot.id);
+  }
+
+  closeModal(){
+    this.setState({modalShown: false});
+    this.getSpots(this.props.target);
+  }
+
+  render(){
+    return  (
+      <Popover placement="top"
+      positionLeft={window.innerWidth/2 - 138} positionTop={window.innerHeight/2 - 150} 
+      title="Parking Near Moscone Center">
+        <div className="row">
+          <div className="col-xs-6 text-center">
+            <h6>Spot No.</h6>
+            <p>{this.props.spot.id}</p>
+          </div>
+          <div className="col-xs-6 text-center">
+            <h6>Cost</h6>
+            <p>
+              <span>
+                {this.props.spot.cost_per_minute}
+              </span>
+              <span>/min</span>
+            </p>
+          </div>
+          <button 
+            className="btn btn-default center-block" 
+            onClick={ this.reserveSpot.bind(this) }>Pay and Reserve</button>
+          <ConfirmModal modalShown={this.state.modalShown} closeModal={this.closeModal.bind(this)} />
+        </div>
+      </Popover>
+    );
+  }
+}
